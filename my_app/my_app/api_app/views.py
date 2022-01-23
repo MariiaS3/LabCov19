8a4420e5ac8db5da91cbcd9d44201f49279fda30
MariiaS3 <mariia.sydor28082001@gmail.com>
2022-01-23 12:53:25 +0100

    logowanie

--- my_app/my_app/api_app/views.py
+++ my_app/my_app/api_app/views.py
@@ -14,7 +14,9 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
-
+from django.shortcuts import render, redirect 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 class Login(generics.GenericAPIView):
     # get method handler
@@ -24,9 +26,32 @@ class Login(generics.GenericAPIView):
     def post(self, request, *args, **kwargs):
         serializer_class = UserLoginSerializer(data=request.data)
         if serializer_class.is_valid(raise_exception=True):
-            return Response(serializer_class.data, status=HTTP_200_OK)
-        return Response(serializer_class.errors, status=HTTP_400_BAD_REQUEST)
+            return render(request, "signin.html",{'data':serializer_class.data})#Response(serializer_class.data, status=HTTP_200_OK)
+        return render(request, "signin.html",{})#Response(serializer_class.errors, status=HTTP_400_BAD_REQUEST)
+        
 
+        #     return render(request, "signin.html",{'data': serializer_class.data})#Response(serializer_class.data, status=HTTP_200_OK)
+        # return Response(serializer_class.errors, status=HTTP_400_BAD_REQUEST)
+
+# def loginPage(request):
+# 	if request.user.is_authenticated:
+# 		return redirect('home')
+# 	else:
+# 		if request.method == 'POST':
+# 			username = request.POST.get('email')
+# 			password =request.POST.get('password')
+
+# 			user = authenticate(request, username=username, password=password)
+
+# 			if user is not None:
+# 				login(request, user)
+# 				return redirect('home')
+# 			else:
+# 				messages.info(request, 'Username OR password is incorrect')
+
+# 		context = {}
+# 		return render(request, 'signin.html', context)
+        
 class Logout(generics.GenericAPIView):
     queryset = Nurse.objects.all()
     serializer_class = UserLogoutSerializer
@@ -49,16 +74,6 @@ class NurseViews(viewsets.ModelViewSet):
     serializer_class = NurseSerializer
     # permission_classes = [permissions.IsAuthenticated]'
 
-# class PatientViews(viewsets.ModelViewSet):
-#     queryset = Patient.objects.all()
-#     authentication_classes = []
-#     serializer_class = PatientSerializer
-    # permission_classes = [permissions.IsAuthenticated]
-#
-# class SpecializationViews(viewsets.ModelViewSet):
-#     queryset = Specialization.objects.all()
-#     authentication_classes = []
-#     serializer_class = SpecalizationSerializer
 
 class VisitViews(viewsets.ModelViewSet):
     queryset = Visit.objects.all()
@@ -73,11 +88,12 @@ def widok(request,*args,**kwargs):
     print("Zalogowany jako: ", request.user)
     return HttpResponse("<h1>Widok <br> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</h1>")
 
+    
 
 
 def template(request,*args,**kwargs):
     print("Zalogowany jako: ", request.user)
-    return render(request, "stronka.html",{})
+    return render(request, "signin.html",{})
 
 
 
