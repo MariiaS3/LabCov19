e90eb40574f75a446abd3f83fd1effa32ba3a9c5
MariiaS3 <mariia.sydor28082001@gmail.com>
2022-01-28 23:26:20 +0100

    mail send plus update index

--- my_app/my_app/api_app/views.py
+++ my_app/my_app/api_app/views.py
@@ -197,7 +197,7 @@ def NewVisit(request):
             form.save(commit=True) #zapisz do bazy
             email = form.cleaned_data['email']
             name = form.cleaned_data['first_name']
-            date = str(form.cleaned_data['data'])
+            date = str(form.cleaned_data['date'])
             task = task_send_email('Your new visit','Hi '+name+',\n\n'+'Thank you for registering in LabCov19.\n The day of your visit is '+date+ '.\n\nWe hope your visit will be nice.\n Feel free to ask any questions.\n',email)
             form=VisitForm() # refresh
             messages.success(request, 'Successfully registered.')
