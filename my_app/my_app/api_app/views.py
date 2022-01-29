087a323047b6426f16fb857ebbfcb499a35bb237
MariiaS3 <mariia.sydor28082001@gmail.com>
2022-01-29 18:04:46 +0100

    send results

--- my_app/my_app/api_app/views.py
+++ my_app/my_app/api_app/views.py
@@ -1,76 +1,31 @@
-
-from atexit import register
-from socket import timeout
 from django.conf import settings
-from rest_framework import  viewsets
-from .serializers import  NurseSerializer, VisitSerializer
-from .models import  Nurse, Visit
+from .serializers import  NurseSerializer
+from .models import  Nurse, Results, Visit
 from .serializers import MyTokenObtainPairSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.http import HttpResponse  
 from .task import task_send_email
-import json
-from django.core.mail import send_mail
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 from rest_framework import generics
 from django.shortcuts import render, redirect 
-from django.contrib.auth import  login, logout
 from django.contrib import messages
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.db.models import Q
-from .forms import VisitForm
+from .forms import ResultsForm, VisitForm
 import re
-from django.shortcuts import get_object_or_404
-from datetime import datetime
-
-class Login(generics.GenericAPIView):
-
-    def get(self, request, *args, **kwargs):
-        return render(request, "signin.html",{})#Response(serializer_class.errors, status=HTTP_400_BAD_REQUEST)
-
-    def post(self, request, *args, **kwargs):
-        if request.method == 'POST':
-            email = request.POST.get('email')
-            password =request.POST.get('password')
-            print(email)
-            user = None
-                # if the email has been passed
-            if '@' in email:
-                user = Nurse.objects.filter(Q(email=email) & Q(password=password)).distinct()
-                if not user.exists():
-                    messages.info(request, 'Email OR password is incorrect')
-                    return  render(request, 'signin.html', {})
-                user = Nurse.objects.get(email=email)
-            else:
-                messages.info(request, 'Email OR password is incorrect')
-                return  render(request, 'signin.html', {})
-            refresh = RefreshToken.for_user(user)
-            user.token = str(refresh.access_token)
-            user.is_active = True
-            user.save()
-            print(user.token)
-            return render(request, 'cookies.html', {'token': user.token})
-        return render(request, 'signin.html', {})
 
 
-
-def Logout(request):
+#main page 
+def main(request,*args,**kwargs):
     user =  Nurse.objects.filter(is_active=True) 
     if user:
         nurse =  Nurse.objects.get(is_active=True) 
-        nurse.is_active = False
-        nurse.token = ""
-        nurse.save()
-    return redirect('login')
-  
+        return render(request, "index.html", {'nurse': nurse})
+    return render(request, "index.html",{})
 
 
-class MyObtainTokenPairView(TokenObtainPairView):
-    permission_classes = (AllowAny,)
-    serializer_class = MyTokenObtainPairSerializer
-
+#check password
 def password_check(password):
  length_error = len(password) < 8 #
  digit_error = re.search(r"\d", password) is None 
@@ -88,7 +43,8 @@ def password_check(password):
  'symbol_error' : symbol_error,
  }
 
-class NurseViews(generics.GenericAPIView):
+# Register
+class Register(generics.GenericAPIView):
     queryset = Nurse.objects.all()
     serializer_class = NurseSerializer
 
@@ -135,30 +91,120 @@ class NurseViews(generics.GenericAPIView):
                 return render(request, 'signup.html', {})
         return render(request, 'signup.html', {})
 
+#login
+class Login(generics.GenericAPIView):
 
-
-class VisitViews(generics.GenericAPIView):
-    queryset = Visit.objects.all()
-    serializer_class = VisitSerializer
-     
     def get(self, request, *args, **kwargs):
-        return render(request, "signup.html", {})
+        return render(request, "signin.html",{})
 
     def post(self, request, *args, **kwargs):
         if request.method == 'POST':
-            print(request.data)
-            serializer_class = VisitSerializer(data=request.data)
-            if serializer_class.is_valid(raise_exception=True):
-                serializer_class.save()
-                messages.success(request, '.....')
-                return  redirect('login')
+            email = request.POST.get('email')
+            password =request.POST.get('password')
+            print(email)
+            user = None
+            if '@' in email:
+                user = Nurse.objects.filter(Q(email=email) & Q(password=password)).distinct()
+                if not user.exists():
+                    messages.info(request, 'Email OR password is incorrect')
+                    return  render(request, 'signin.html', {})
+                user = Nurse.objects.get(email=email)
             else:
-                return render(request, 'signup.html', {})
-        return render(request, 'signup.html', {})
+                messages.info(request, 'Email OR password is incorrect')
+                return  render(request, 'signin.html', {})
+            refresh = RefreshToken.for_user(user)
+            user.token = str(refresh.access_token)
+            user.is_active = True
+            user.save()
+            print(user.token)
+            return render(request, 'cookies.html', {'token': user.token})
+        return render(request, 'signin.html', {})
 
-    # def get_queryset(self):
-    #     spec_id = self.request.query_params.get('spec_id')
-    #     return Visit.objects.filter(doctor__specialization__id = spec_id).distinct()
+#logout
+def Logout(request):
+    user =  Nurse.objects.filter(is_active=True) 
+    if user:
+        nurse =  Nurse.objects.get(is_active=True) 
+        nurse.is_active = False
+        nurse.token = ""
+        nurse.save()
+    return redirect('login')
+  
+#list of patient visits
+def visitsList(request):
+    user =  Nurse.objects.filter(is_active=True) 
+    if user:
+        nurse =  Nurse.objects.get(is_active=True)
+        email = list(request.GET)
+        if email == []:
+            visits = Visit.objects.all()
+            return render(request, 'visits.html', {'visits':visits, 'nurse': nurse})
+        else:
+            Results.objects.all().delete()
+            visit = Visit.objects.get(email=email[0])
+            Results.objects.create(email=visit.email, first_name=visit.first_name,last_name=visit.last_name,phone_number=visit.phone_number,vaccine=visit.do_you_want_vaccine) 
+            Visit.objects.filter(email=email[0]).delete()          
+            return redirect( 'results')
+    return render(request, 'visits.html', {})
+
+#Register for a new visit
+def NewVisit(request):
+    form = VisitForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save(commit=True) #zapisz do bazy
+            email = form.cleaned_data['email']
+            name = form.cleaned_data['first_name']
+            date = str(form.cleaned_data['date'])
+            task_send_email('Your new visit','Hi '+name+',\n\n'+'Thank you for registering in LabCov19.\n The day of your visit is '+date+ '.\n\nWe hope your visit will be nice.\n Feel free to ask any questions.\n',email)
+            form=VisitForm() # refresh
+            messages.success(request, 'Successfully registered.')
+        else:
+            messages.error(request, 'Something wrong')
+    
+    context = {
+        'form' : form
+    }
+
+    return render(request,"newvisit.html", context)
+
+#send test results
+def SendResults(request):
+    form = ResultsForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            name = form.cleaned_data['first_name']
+            test = form.cleaned_data['results']
+            vaccine = form.cleaned_data['vaccine']
+            message = form.cleaned_data['messenge']
+            if test == 'N' and vaccine=='Y':
+                task_send_email('Results','Hello '+name+',\nYour test your test is ready, your test result is: NEGATIVE \n Thank you for vaccinating with us.\nYou also have a message from the nurse '+message,email)
+            elif test == 'N' and vaccine=='N':
+                task_send_email('Results','Hello '+name+',\nYour test your test is ready, your test result is: NEGATIVE \n We encourage you to vaccinate yourself.\nYou also have a message from the nurse '+message,email)
+            elif test == 'P':
+                task_send_email('Results','Hello '+name+',\nYour test your test is ready, your test result is: POSITIVE \nWe wish you a quick recovery and encourage you to vaccinate yourself.\nYou also have a message from the nurse '+message,email)
+            return redirect("visits")
+        else:
+            messages.error(request, 'Something wrong')
+
+    visit = list(Results.objects.all())
+    if visit==[]:
+        print(visit)
+    else:
+        form = ResultsForm(initial={'email': visit[0].email,'first_name':visit[0].first_name,'last_name':visit[0].last_name,'phone_number':visit[0].phone_number,'vaccine':visit[0].vaccine})
+    context = {
+        'form' : form
+    }
+    return render(request,"results.html", context)
+    
+
+
+#=================================================================#
+class MyObtainTokenPairView(TokenObtainPairView):
+    permission_classes = (AllowAny,)
+    serializer_class = MyTokenObtainPairSerializer
+
 
 def widok(request,*args,**kwargs):
     print("Zalogowany jako: ", request.user)
@@ -170,42 +216,3 @@ def template(request,*args,**kwargs):
     return render(request, "signin.html",{})
 
 
-def visitsList(request):
-    user =  Nurse.objects.filter(is_active=True) 
-    if user:
-        nurse =  Nurse.objects.get(is_active=True) 
-        print(nurse) 
-        visits = Visit.objects.all()
-        return render(request, 'visits.html', {'visits':visits, 'nurse': nurse})
-    return render(request, 'visits.html', {})
-    
-
-def main(request,*args,**kwargs):
-    user =  Nurse.objects.filter(is_active=True) 
-    if user:
-        nurse =  Nurse.objects.get(is_active=True) 
-        print(nurse) 
-        print("Zalogowany jako: ", request.user)
-        return render(request, "index.html", {'nurse': nurse})
-    return render(request, "index.html",{})
-
-
-def NewVisit(request):
-    form = VisitForm(request.POST or None)
-    if request.method == 'POST':
-        if form.is_valid():
-            form.save(commit=True) #zapisz do bazy
-            email = form.cleaned_data['email']
-            name = form.cleaned_data['first_name']
-            date = str(form.cleaned_data['date'])
-            task = task_send_email('Your new visit','Hi '+name+',\n\n'+'Thank you for registering in LabCov19.\n The day of your visit is '+date+ '.\n\nWe hope your visit will be nice.\n Feel free to ask any questions.\n',email)
-            form=VisitForm() # refresh
-            messages.success(request, 'Successfully registered.')
-        else:
-            messages.error(request, 'Something wrong')
-    
-    context = {
-        'form' : form
-    }
-
-    return render(request,"newvisit.html", context)
