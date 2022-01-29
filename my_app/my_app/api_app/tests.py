843e2c6c81870ed62902a4bdeffbecb3c42a20b3
sarneusz <mateusz.gruszkiewicz@wp.pl>
2022-01-29 22:42:47 +0100

    tests

--- my_app/my_app/api_app/tests.py
+++ my_app/my_app/api_app/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from api_app.models import Nurse, Visit
+# Create your tests here. ❯ python3 manage.py test
 
-# Create your tests here.
+class NurseTestCase(TestCase):
+    def setUp(self):
+        self.nurse = Nurse(username="Mateusz",email="mateusz@dupa.pl", phone_number="123456789")
+
+    def test_create_nurse(self): #czy tworzy nurse
+        self.nurse.save()
+        self.assertIsNotNone(self.nurse.id) #czy id jest rozne od 0
+
